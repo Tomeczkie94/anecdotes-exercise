10638cba3522b13e64341797bc9e55a8cf79e4f7
Tomasz Jakóbiec <tomaszjakobiec94@gmail.com>
2020-06-28 06:58:22 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Starter code</h1>
+    </div>
+  )
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
